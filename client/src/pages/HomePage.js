6b382fc95372e5dd66f4c492e4ab294a96524fc2
nikola chakarov <chakarovn1975@gmail.com>
2023-07-21 07:01:29 +0300

    setup graph client

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,16 @@
 import JobList from '../components/JobList';
 import { jobs } from '../lib/fake-data';
+import { getJobs } from '../lib/graphql/queries';
+
+getJobs().then((res) => console.log(res));
 
 function HomePage() {
-  return (
-    <div>
-      <h1 className="title">
-        Job Board
-      </h1>
-      <JobList jobs={jobs} />
-    </div>
-  );
+	return (
+		<div>
+			<h1 className='title'>Job Board</h1>
+			<JobList jobs={jobs} />
+		</div>
+	);
 }
 
 export default HomePage;
