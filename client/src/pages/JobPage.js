c03317ba3125c595ee798cb9ce2840cdaa1060ab
nikola chakarov <chakarovn1975@gmail.com>
2023-07-21 18:53:30 +0300

    graph front setup...

--- client/src/pages/JobPage.js
+++ client/src/pages/JobPage.js
@@ -2,31 +2,41 @@ import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { formatDate } from '../lib/formatters';
 import { jobs } from '../lib/fake-data';
+import { getJob } from '../lib/graphql/queries';
+import { useEffect, useState } from 'react';
 
 function JobPage() {
-  const { jobId } = useParams();
+	const { jobId } = useParams();
 
-  const job = jobs.find((job) => job.id === jobId);
-  return (
-    <div>
-      <h1 className="title is-2">
-        {job.title}
-      </h1>
-      <h2 className="subtitle is-4">
-        <Link to={`/companies/${job.company.id}`}>
-          {job.company.name}
-        </Link>
-      </h2>
-      <div className="box">
-        <div className="block has-text-grey">
-          Posted: {formatDate(job.date, 'long')}
-        </div>
-        <p className="block">
-          {job.description}
-        </p>
-      </div>
-    </div>
-  );
+	const [job, setJob] = useState(null);
+	useEffect(() => {
+		getJob(jobId).then((res) => {
+			console.log(res);
+			setJob(res);
+		});
+	}, [jobId]);
+
+	if (!job) {
+		return <p>Loading...</p>;
+	}
+
+	return (
+		<div>
+			<h1 className='title is-2'>{job?.title}</h1>
+			<h2 className='subtitle is-4'>
+				<Link to={`/companies/${job?.company?.id}`}>
+					{job?.company?.name}
+				</Link>
+			</h2>
+			<div className='box'>
+				<div className='block has-text-grey'>
+					{/* Posted: {formatDate(job?.date, 'long')} */}
+					Posted: {job.date}
+				</div>
+				<p className='block'>{job?.description}</p>
+			</div>
+		</div>
+	);
 }
 
 export default JobPage;
